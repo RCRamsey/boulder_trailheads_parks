72acf3a14332522f1c8fb661de2c667d1f0adf7d
RCRamsey <72559630+RCRamsey@users.noreply.github.com>
2021-11-08 06:04:16 -0700

    npm inititate

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict"
+
+//require funx looks for package/module of that name within node_modules/directory
+//and then assigns to var chalk
+var chalk = require('chalk');
+
+//console.log('hello node');
+console.log(chalk.green("hello node"));
